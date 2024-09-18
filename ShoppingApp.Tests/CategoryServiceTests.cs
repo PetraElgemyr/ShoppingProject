085562eedd09d8a60232f6ec5d0105e21b0ca7f0
Petra <petra.elgemyr@gmail.com>
2024-09-18 18:11:34 +0200

    add test project

---
+++ ShoppingApp.Tests/CategoryServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingApp.Services;
+
+namespace ShoppingApp.Tests;
+
+public class CategoryServiceTests
+{
+    //UserService userService = new UserService();
+    CategoryService categoryService = new CategoryService();
+
+    [Fact]
+    public void GetAllCategories__ShouldGetAllCategoriesFromList_ReturnResponseWithContent()
+    {
+
+    }
+}
