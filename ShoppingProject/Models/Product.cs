fe1c06705d25c7f32eb88f2f5fc218bc4c591ef6
Petra <petra.elgemyr@gmail.com>
2024-09-19 09:02:19 +0200

    make model properties optional/nullable

--- ShoppingProject/Models/Product.cs
+++ ShoppingProject/Models/Product.cs
@@ -7,6 +7,6 @@ public class Product : IProduct
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = "";
     public int Price { get; set; }
-    public string Description { get; set; } = "";
-    public string CategoryId { get; set; } = "";
+    public string? Description { get; set; } 
+    public string? CategoryId { get; set; } 
 }
