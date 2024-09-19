277347e8e3932489d57861f98cb2d23e7028cd31
Petra <petra.elgemyr@gmail.com>
2024-09-19 09:01:40 +0200

    add service interfaces

---
+++ ShoppingProject/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingApp.Models;
+
+namespace ShoppingApp.Interfaces;
+
+internal interface IProductService
+{
+    public Response GetAllProducts();
+    public Response AddNewProductToList(Product productRequest);
+    public Response UpdateProductById(Product updatedProduct);
+    public Response DeleteProductById(string id);
+    public Response GetAllProductsByCategoryId(string categoryId);
+
+    public Response DeleteProductsByCategoryId(string categoryId);
+    
+}
