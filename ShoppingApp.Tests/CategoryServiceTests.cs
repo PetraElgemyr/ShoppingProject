6437cc69c0d9094cbb2554e06af4873220212f70
Petra <petra.elgemyr@gmail.com>
2024-09-20 02:33:19 +0200

    new project reference to class library and remove old service instantiation (sv instansiering)

--- ShoppingApp.Tests/CategoryServiceTests.cs
+++ ShoppingApp.Tests/CategoryServiceTests.cs
@@ -1,11 +1,7 @@
-﻿using ShoppingApp.Services;
-
-namespace ShoppingApp.Tests;
+﻿namespace ShoppingApp.Tests;
 
 public class CategoryServiceTests
 {
-    //UserService userService = new UserService();
-    CategoryService categoryService = new CategoryService();
 
     [Fact]
     public void GetAllCategories__ShouldGetAllCategoriesFromList_ReturnResponseWithContent()
