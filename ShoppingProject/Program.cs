dc63e6dfc5b5499278c4f35c3dfc6f132d67a12b
Petra <petra.elgemyr@gmail.com>
2024-09-20 02:45:15 +0200

    add test files created by test runs, add reference to shoppingapp to class library

--- ShoppingProject/Program.cs
+++ ShoppingProject/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ShoppingApp.Menus;
 
+//ICategoryService<Category, ICategory> categoryService = new CategoryService(@"C:\Skola\Nackademin\Programmering-sharp\ShoppingProject\categories.json");
+//IProductService<Product, IProduct> productService = new ProductService(@"C:\Skola\Nackademin\Programmering-sharp\ShoppingProject\products.json");
+
 var menuService = new MainMenu();
 
 while (true)
