88f37427f4bc0ba6ff82018d84f72b59df36cc12
Petra <petra.elgemyr@gmail.com>
2024-09-20 02:30:21 +0200

    add new class library w interfaces and new structure

---
+++ Resources/Interfaces/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using Resources.Models;
+
+namespace Resources.Interfaces;
+
+public interface IFileService
+{
+    public Response<string> SaveToFile(string content);
+
+    public Response<string> GetFromFile();
+
+}
+
+
