cae27999e6136399e64c83e380df6101f06b37f5
Petra <petra.elgemyr@gmail.com>
2024-10-06 21:50:24 +0200

    add test file for productService

---
+++ ShoppingApp.Tests/ProductServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using Resources.Interfaces;
+using Resources.Models;
+
+namespace ShoppingApp.Tests;
+
+public class ProductServiceTests
+{
+
+    private readonly Mock<IProductService<Product, IProduct>> _mockProductService = new();
+
+    [Fact]
+    public void GetAllProducts__ShouldGetAllProductsFromList_ReturnResponseWithContent()
+    {
+
+    }
+}
